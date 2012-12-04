0a4f44815f10be2cac8f1a17698f60337fcff1aa
Mathieu Ledru <matyo91@gmail.com>
2012-12-04 13:01:16 +0100

    svn add -u

--- node/MatyoTools/src/exec/svn/add.js
+++ node/MatyoTools/src/exec/svn/add.js
@@ -1,5 +1,21 @@
 define(['matyotools', 'exec/svn'], function(matyotools) {
     matyotools.exec.childs.svn.childs.add = function(argv) {
-        
+        var fs = require('fs-extra');
+
+        if(fs.existsSync('.svn')) {
+            var program = require('commander');
+            var execSync = require('exec-sync');
+
+            program
+                .version('0.0.1')
+                .option('-u, --unversioned', 'add unversionned files')
+                .parse(argv);
+
+            if(program.unversioned) {
+                console.log(execSync('svn st | grep ^\? | awk {\'print "svn add "\$2\'} | sh'));
+            }
+        } else {
+            console.log("svn: warning: '.' is not a working copy");
+        }
     };
 });
