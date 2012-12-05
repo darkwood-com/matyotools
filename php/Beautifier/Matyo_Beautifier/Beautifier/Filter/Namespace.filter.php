1b2828e349845e1584f98ebad5370e731a3d3441
Mathieu Ledru <matyo91@gmail.com>
2012-12-05 16:02:53 +0100

    fsm

--- php/Beautifier/Matyo_Beautifier/Beautifier/Filter/Namespace.filter.php
+++ php/Beautifier/Matyo_Beautifier/Beautifier/Filter/Namespace.filter.php
@@ -5,14 +5,35 @@ require_once 'FSM/FSM.php';
 class PHP_Beautifier_Filter_Namespace extends PHP_Beautifier_Filter
 {
     private $fsm = null;
+    private $stack = array();
 
     public function __construct(PHP_Beautifier $oBeaut, $aSettings = array())
     {
         parent::__construct($oBeaut, $aSettings);
 
         //FSM that detect namespaces
+        $this->fsm = new FSM('NONE', $this->stack);
+        $this->fsm->addTransition('t_include', 'NONE', 'INCLUDE');
+        $this->fsm->addTransition('t_whitespace', 'INCLUDE', 'WHITESPACE');
+        $this->fsm->addTransition('t_string', 'WHITESPACE', 'STRING');
+        $this->fsm->addTransition('t_semi_colon', 'STRING', 'NONE', array($this, 'fsm_end'));
+
+        $this->fsm->addTransitionAny('NONE', 'NONE', array($this, 'fsm_reset'));
+        $this->fsm->addTransitionAny('INCLUDE', 'NONE', array($this, 'fsm_reset'));
+        $this->fsm->addTransitionAny('WHITESPACE', 'NONE', array($this, 'fsm_reset'));
+        $this->fsm->addTransitionAny('STRING', 'NONE', array($this, 'fsm_reset'));
+
+        //$this->fsm->addTransition('T_STRING', 'USE_NS_SEPARATOR', 'USE_NAMESPACE');
+        //$this->fsm->addTransition('T_NS_SEPARATOR', 'USE_NAMESPACE', 'USE_NS_SEPARATOR');
+    }
+
+    public function fsm_reset($symbol, $stack)
+    {
         $stack = array();
-        $this->fsm = new FSM('start', $stack);
+    }
+
+    public function fsm_end($symbol, $stack)
+    {
     }
 
     public function __call($sMethod, $aArgs)
@@ -21,22 +42,15 @@ class PHP_Beautifier_Filter_Namespace extends PHP_Beautifier_Filter
             throw (new Exception('Call to Filter::__call with wrong argument'));
         }
 
-        return PHP_Beautifier_Filter::BYPASS;
-    }
+        array_push($this->stack, $aArgs[0]);
+        $this->fsm->process($sMethod);
 
-    public function t_include($tag)
-    {
-        return PHP_Beautifier_Filter::BYPASS;
-    }
-
-    public function t_ns_separator($tag)
-    {
         return PHP_Beautifier_Filter::BYPASS;
     }
 
     public function postProcess()
     {
-
+        print_r($this->stack);
     }
 }
 ?>
