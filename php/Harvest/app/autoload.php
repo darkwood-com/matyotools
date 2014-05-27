2c6cc3bdabdefda530c61f8791d593f81425189d
Mathieu Ledru <matyo91@gmail.com>
2014-05-27 10:59:14 +0000

    harvest

--- php/Harvest/app/autoload.php
+++ php/Harvest/app/autoload.php
@@ -10,4 +10,7 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
+require_once __DIR__.'/../vendor/mdbitz/hapi/HarvestAPI.php';
+spl_autoload_register( array('HarvestAPI', 'autoload') );
+
 return $loader;
