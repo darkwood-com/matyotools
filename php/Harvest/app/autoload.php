36cebc30822361dc260b9320d96bcc2788dd5e35
Mathieu Ledru <matyo91@gmail.com>
2014-05-27 10:40:19 +0200

    Harvest

--- php/Harvest/app/autoload.php
+++ php/Harvest/app/autoload.php
@@ -1,18 +1,12 @@
 <?php
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
 
+/**
+ * @var ClassLoader $loader
+ */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
-// intl
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
-}
-
-require_once __DIR__.'/../vendor/mdbitz/hapi/HarvestAPI.php';
-spl_autoload_register( array('HarvestAPI', 'autoload') );
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
