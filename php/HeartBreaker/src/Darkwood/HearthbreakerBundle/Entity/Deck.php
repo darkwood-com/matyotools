3b608aac9317441544b51e8e068ec268fe3ea4ed
Mathieu Ledru <matyo91@gmail.com>
2015-03-22 12:10:42 +0100

    db

---
+++ php/HeartBreaker/src/Darkwood/HearthbreakerBundle/Entity/Deck.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Darkwood\HearthbreakerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Deck
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Deck
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=255)
+     */
+    private $slug;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Deck
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Deck
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
