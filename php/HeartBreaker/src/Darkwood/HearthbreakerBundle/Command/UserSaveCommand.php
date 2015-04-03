839953f5cd76a73f6cce19ade96e37dfb50c3aa5
Mathieu Ledru <matyo91@gmail.com>
2015-04-03 10:04:57 +0200

    save

--- php/HeartBreaker/src/Darkwood/HearthbreakerBundle/Command/UserSaveCommand.php
+++ php/HeartBreaker/src/Darkwood/HearthbreakerBundle/Command/UserSaveCommand.php
@@ -18,6 +18,43 @@ class UserSaveCommand extends ContainerAwareCommand
         ;
     }
 
+	private function jsonToReadable($json){
+		$tc = 0;        //tab count
+		$r = '';        //result
+		$q = false;     //quotes
+		$t = "\t";      //tab
+		$nl = "\n";     //new line
+
+		for($i=0;$i<strlen($json);$i++){
+			$c = $json[$i];
+			if($c=='"' && $json[$i-1]!='\\') $q = !$q;
+			if($q){
+				$r .= $c;
+				continue;
+			}
+			switch($c){
+				case '{':
+				case '[':
+					$r .= $c . $nl . str_repeat($t, ++$tc);
+					break;
+				case '}':
+				case ']':
+					$r .= $nl . str_repeat($t, --$tc) . $c;
+					break;
+				case ',':
+					$r .= $c;
+					if($json[$i+1]!='{' && $json[$i+1]!='[') $r .= $nl . str_repeat($t, $tc);
+					break;
+				case ':':
+					$r .= $c . ' ';
+					break;
+				default:
+					$r .= $c;
+			}
+		}
+		return $r;
+	}
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 		$container = $this->getContainer();
@@ -40,6 +77,7 @@ class UserSaveCommand extends ContainerAwareCommand
 
 		$encoder = new JsonEncoder();
 		$json = $encoder->encode($data, JsonEncoder::FORMAT);
+		$json = $this->jsonToReadable($json);
 
 		/** @var \Symfony\Component\HttpKernel\Kernel $kernel */
 		$kernel = $container->get('kernel');
