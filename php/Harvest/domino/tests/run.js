a2df891608d277d2e2da01266908e7854c51dad7
Mathieu Ledru <matyo91@gmail.com>
2015-06-10 21:50:01 +0200

    test

--- php/Harvest/domino/tests/run.js
+++ php/Harvest/domino/tests/run.js
@@ -2,15 +2,18 @@ module.exports = {
 	'Test domino' : function (browser) {
 		var params = browser.globals.test_settings.globals;
 
+        var wait = 5000, saisieDesTempsPath = '#menu_3';
+
 		browser
 			.url('https://dominoweb.domino-info.fr:7001/cgiphl/pw_main.pgm')
-			.waitForElementVisible('body', 1000)
+			.waitForElementVisible('body', wait)
 			.setValue('input[name="name1"]', params.user)
 			.setValue('input[name="name2"]', params.password)
-			.waitForElementVisible('input[type="button"][value="OK"]', 1000)
+			.waitForElementVisible('input[type="button"][value="OK"]', wait)
 			.click('input[type="button"][value="OK"]')
-			.pause(1000)
-			.assert.containsText('#main', 'Night Watch')
+            .waitForElementVisible(saisieDesTempsPath, wait)
+			.click(saisieDesTempsPath)
+            .pause(wait)
 			.end();
 	}
 };
