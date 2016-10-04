a7afdfb5a1a64a19f66a8b1a583cf9d7791f0e6d
Mathieu Ledru <matyo91@gmail.com>
2016-10-04 18:04:53 +0200

    homepage

--- php/SocialNetwork/src/AppBundle/Controller/HomeController.php
+++ php/SocialNetwork/src/AppBundle/Controller/HomeController.php
@@ -6,19 +6,40 @@ use Facebook\Facebook;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function homeAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Home:index.html.twig', array(
+        ));
+    }
+
+    /**
+     * @Route("/fb-login", name="facebookLogin")
+     */
+    public function facebookLogin(Request $request)
+    {
+
+    }
+
+    public function menuAction()
     {
         /** @var Facebook $facebook */
         $facebook = $this->get('app.facebook');
-        dump($facebook);
+
+        $helper = $facebook->getRedirectLoginHelper();
+        $permissions = ['email', 'user_likes']; // optional
+        $loginUrl = $helper->getLoginUrl($this->generateUrl('facebookLogin', array(), UrlGeneratorInterface::ABSOLUTE_URL), $permissions);
 
         // replace this example code with whatever you need
-        return $this->render('AppBundle:Home:index.html.twig');
+        return $this->render('AppBundle:partials:menu.html.twig', array(
+            'loginUrl' => $loginUrl,
+        ));
     }
 }
