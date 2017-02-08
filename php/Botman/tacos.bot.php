69e1ff624907f1d843869f7a89afffef95cd69cc
Mathieu Ledru <matyo91@gmail.com>
2017-02-08 14:40:49 +0100

    🙂 send-taco

--- php/Botman/tacos.bot.php
+++ php/Botman/tacos.bot.php
@@ -6,16 +6,28 @@ require 'vendor/autoload.php';
 use Mpociot\BotMan\BotManFactory;
 use Mpociot\BotMan\BotMan;
 use React\EventLoop\Factory;
+use Slack\ApiClient;
+use Slack\Channel;
+use Slack\User;
 
 $loop = Factory::create();
-$botman = BotManFactory::createForRTM($config, $loop);
+//$botman = BotManFactory::createForRTM($config, $loop);
 
-$botman->hears('keyword', function(BotMan $bot) {
-    $bot->reply('I heard you! :)');
-});
+$client = new ApiClient($loop);
+$client->setToken($config['slack_token']);
+$client->getChannelByName('general')->then(function($channel) use ($client) {
+    /** @var Channel $channel */
+    $channel->getMembers()->then(function($members) use ($client, $channel) {
+        /** @var User[] $members */
 
-$botman->hears('convo', function(BotMan $bot) {
-    $bot->startConversation(new ExampleConversation());
+        //get 5 members
+        shuffle($members);
+        $members = array_slice($members, 0, 5);
+        foreach ($members as $member)
+        {
+            $client->send("@{$member->getUsername()} :taco:", $channel);
+        }
+    });
 });
 
 $loop->run();
