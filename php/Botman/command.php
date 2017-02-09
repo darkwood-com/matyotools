43db7b1f7b1ae70810e764eb4287d5c4a0ade9de
Mathieu Ledru <matyo91@gmail.com>
2017-02-09 17:52:22 +0100

    🙂 use autoload psr0

--- php/Botman/command.php
+++ php/Botman/command.php
@@ -2,8 +2,6 @@
 
 require __DIR__.'/config.php';
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/src/bots/autojoin.php';
-require __DIR__.'/src/bots/tacos.php';
 
 use Symfony\Component\Console\Application;
 use bots\AutoJoinCommand;
