cfc347099b801f70350c73e40731339401d8690c
Mathieu Ledru <matyo91@gmail.com>
2017-02-17 17:00:46 +0100

    😊 react command

---
+++ php/Botman/src/Command/ReactCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Command;
+
+use Services\SlackService;
+use Slack\Group;
+use Slack\Message\Message;
+use Slack\Message\MessageBuilder;
+use Slack\Payload;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Mpociot\BotMan\BotManFactory;
+use Mpociot\BotMan\BotMan;
+use React\EventLoop\Factory;
+use React\Promise;
+use Slack\ApiClient;
+use Slack\Channel;
+use Slack\User;
+
+class ReactCommand extends Command
+{
+    /**
+     * @var SlackService
+     */
+    protected $slackService;
+
+    public function __construct($slackService)
+    {
+        parent::__construct();
+
+        $this->slackService = $slackService;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('react');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $loop = Factory::create();
+
+
+
+        $loop->run();
+    }
+}
